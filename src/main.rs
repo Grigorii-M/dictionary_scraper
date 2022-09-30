445174c00de2ec09f5bf6989c5cf99fc93492a1b
Grigorii Matiukhin <grigory36m@gmail.com>
2022-09-30 11:36:30 +0300

    Able to find definitions for the given word

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use scraper::{ Html, Selector };
+use scraper::{Html, Selector};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
@@ -11,27 +11,43 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
     let args = Args::parse();
 
-    let resp = reqwest::get(format!("https://verbformen.de/konjugation/?w={}", args.word))
-        .await?;
-    let responce_headers = resp.headers();
-    println!("{:#?}", responce_headers);
+    let resp = reqwest::get(format!(
+        "https://verbformen.de/konjugation/?w={}",
+        args.word
+    ))
+    .await?;
 
-    let responce_body = resp
-        .text()
-        .await?;
+    let responce_body = resp.text().await?;
 
     let document = Html::parse_document(&responce_body);
-    // println!("{:#?}", document);
 
-    // let selector = Selector::parse("rInf, r1Zeile, ru3px, rO0px, rNt").unwrap();
-    // // println!("{:?}", selector);
-    //
-    // for el in document.select(&selector) {
-    //     println!("{:#?}", el);
-    // }
+    let info_selector = Selector::parse(".rAbschnitt").unwrap();
 
+    // verbformen.de webpage has only one element with .rAbschnitt class
+    let word_info = document.select(&info_selector).next().unwrap();
+
+    let selector1 = Selector::parse(".rInf").unwrap();
+    let selector2 = Selector::parse(".r1Zeile").unwrap();
+    let selector3 = Selector::parse(".rU3px").unwrap();
+    let selector4 = Selector::parse(".rO0px").unwrap();
+    let selector5 = Selector::parse(".rNt").unwrap();
+
+    let definition = word_info
+        .select(&selector1)
+        .find(|el| {
+            selector2.matches(el)
+                && selector3.matches(el)
+                && selector4.matches(el)
+                && selector5.matches(el)
+        })
+        .unwrap();
+    let mut definition_str = String::new();
+    for el in definition.text() {
+        definition_str.push_str(el);
+    }
+
+    println!("{}", definition_str.trim());
     Ok(())
 }
