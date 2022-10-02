2ae32eb8cb382b55112b0fffdbac91613daf1d59
Grigorii Matiukhin <grigory36m@gmail.com>
2022-10-02 23:40:56 +0300

    Added an ability to specify different parts of speech in CLI

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 use scraper::{Html, Selector};
-use scraper::element_ref::Text;
 
-pub async fn find_word(word: &str) -> Result<WordInfo, Box<dyn std::error::Error>> {
+#[derive(Debug)]
+pub enum WordType {
+    Verb,
+    Noun,
+    Adjective,
+}
+
+pub async fn find_word(
+    word: &str,
+    word_type: WordType,
+) -> Result<WordInfo, Box<dyn std::error::Error>> {
     // use `konjugation/<wrd>.htm` for verbs
     // use `deklination/substantive/<wrd>.htm` for nouns
     // use `deklination/adjektive/<wrd>.htm` for adjectives
@@ -15,56 +24,83 @@ pub async fn find_word(word: &str) -> Result<WordInfo, Box<dyn std::error::Error
     // verbformen.de webpage has only one element with .rAbschnitt class
     let full_info = Html::parse_document(&html.select(&info_selector).next().unwrap().html());
 
-    Ok(WordInfo {
-        html: full_info,
-    })
+    match word_type {
+        _ => Ok(WordInfo::VerbInfo(find_verb(&full_info))),
+    }
 }
 
-pub struct WordInfo {
-    html: Html,
+pub enum WordInfo {
+    VerbInfo(VerbInfo),
+    NounInfo,
+    AdjectiveInfo,
 }
 
-impl WordInfo {
-    pub fn definition(&self) -> String {
-        let brief_info_selector = Selector::parse("#vVdBxBox").unwrap();
-        let brief_info = self.html.select(&brief_info_selector).next().unwrap();
+pub struct VerbInfo {
+    pub meta_info: String,
+    pub verb_forms: String,
+    pub translation: String,
+    pub definition: String,
+    pub usage: String,
+    pub example: String,
+}
 
-        let gr_info_selector = Selector::parse(".rInf").unwrap();
-        let gr_info = brief_info.select(&gr_info_selector).next().unwrap();
+pub fn find_verb(html: &Html) -> VerbInfo {
+    let brief_info_selector = Selector::parse("#vVdBxBox").unwrap();
+    let brief_info = html.select(&brief_info_selector).next().unwrap();
 
-        let mut data = Vec::new();
+    let gr_info_selector = Selector::parse(".rInf").unwrap();
+    let gr_info = brief_info.select(&gr_info_selector).next().unwrap();
 
-        data.push(gr_info.text().fold(String::new(), |mut acc, line| {acc = format!("{}{}", acc, line.replace("\n", " ")); acc }));
+    let meta_info = gr_info.text().fold(String::new(), |mut acc, line| {
+        acc = format!("{}{}", acc, line.replace("\n", " "));
+        acc
+    }).trim().to_string();
 
-        let verb_forms_selector = Selector::parse("#stammformen").unwrap();
-        let verb_forms = brief_info.select(&verb_forms_selector).next().unwrap();
+    let verb_forms_selector = Selector::parse("#stammformen").unwrap();
+    let verb_forms = brief_info.select(&verb_forms_selector).next().unwrap();
 
-        data.push(verb_forms.text().fold(String::new(), |mut acc, line| {acc = format!("{}{}", acc, line.replace("\n", " ")); acc }));
+    let verb_forms = verb_forms.text().fold(String::new(), |mut acc, line| {
+        acc = format!("{}{}", acc, line.replace("\n", " "));
+        acc
+    }).trim().to_string();
 
-        // These classes are being used with translation, definition, case and example
-        let selector1 = Selector::parse(".r1Zeile").unwrap();
-        let selector2 = Selector::parse(".rU3px").unwrap();
-        let selector3 = Selector::parse(".rO0px").unwrap();
+    // These classes are being used with translation, definition, case and example
+    let selector1 = Selector::parse(".r1Zeile").unwrap();
+    let selector2 = Selector::parse(".rU3px").unwrap();
+    let selector3 = Selector::parse(".rO0px").unwrap();
 
-        let info = brief_info.select(&selector1).filter_map(|el| {
-            if selector2.matches(&el) && selector3.matches(&el) {
-                let mut data = String::new();
-                el.text().for_each(|line| data.push_str(line));
-                Some(data)
-            } else {
-                None
-            }
-        });
+    let info = brief_info.select(&selector1).filter_map(|el| {
+        if selector2.matches(&el) && selector3.matches(&el) {
+            let mut data = String::new();
+            el.text().for_each(|line| data.push_str(line));
+            Some(data)
+        } else {
+            None
+        }
+    });
 
-        info.for_each(|line| {
-            data.push(line.trim().replace("\n", " "));
-        });
+    // info.for_each(|line| {
+    //     data.push(line.trim().replace("\n", " "));
+    // });
 
-        let mut result = String::new();
-        data.into_iter().for_each(|line| {
-            result.push_str(&line.trim());
-            result.push_str("\n");
-        });
-        result
+    let (translation, definition, usage, example) = {
+        let info = info
+            .map(|el| el.trim().replace("\n", " "))
+            .collect::<Vec<_>>();
+        (
+            info[0].trim().to_string(),
+            info[1].trim().to_string(),
+            info[2].trim().to_string(),
+            info[3].trim().to_string(),
+        )
+    };
+
+    VerbInfo {
+        meta_info,
+        verb_forms,
+        translation,
+        definition,
+        usage,
+        example,
     }
 }
